ea72b6c18c485c98a2b3e37838751c518b66c270
底なし沼の魔女 <megablacklabel@gmail.com>
2018-04-15 13:37:04 +0900

    getInitialProps サンプル実装（クラス構造）

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,16 @@
-export default () => <div>Welcome to next.js!!!</div>
+import React from 'react'
+export default class extends React.Component {
+  // サーバサイド時にgetInitialPropsの処理が実行される
+  static async getInitialProps({ req }) {
+    if (req) {
+      // reqがある(サーバサイド処理)なのでheadersデータを取得
+      const userAgent = req.headers['user-agent'];
+      // 取得したユーザエージェントをクライアントサイドにpropsとして渡す
+      return { userAgent };
+    }
+  }
+  render() {
+    // propsとして受け取ったuserAgentを表示
+    return <div>userAgent: {this.props.userAgent}</div>;
+  }
+}
